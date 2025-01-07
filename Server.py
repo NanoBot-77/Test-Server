928e06edb9ff61d4f59080eb471c459716f56042
NanoBot-77 <nanobotuser@gmail.com>
2025-01-07 12:56:23 +0530

    Updated API

--- Server.py
+++ Server.py
@@ -1,6 +1,8 @@
 import flask
 from flask import Flask, redirect, request, jsonify, render_template_string
 from flask_cors import CORS
+import random
+
 
 app = Flask(__name__)
 
@@ -14,5 +16,15 @@ def Home():
     """
 
 
+@app.route('/api/links')
+def links():
+    return jsonify({'links':{'about':'/api/about', "TestJson":"/api/TestJson"}})
 
-    
+@app.route('/api/about')
+def About():
+    return "This is a test Server  created on python Flask for Testing Nginx and duckdns"
+
+@app.route('/api/TestJson')
+def TestJson():
+    R = random.randint(10, 200)
+    return jsonify({'R_Data':R})
